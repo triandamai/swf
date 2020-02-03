8e6ee8fb7ef6e2f4fffa3d0ea435d17ee00c7d93
Trian Damai <triannurizkillah@gmail.com>
2020-02-03 16:12:32 +0700

    daftar page yooosh

---
+++ SWF-1/app/src/main/java/com/pmo/swf1/ActivityUtama.java
@@ -0,0 +1,14 @@
+package com.pmo.swf1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utama);
+    }
+}
---
+++ SWF-1/app/src/main/java/com/pmo/swf1/Daftar.java
@@ -0,0 +1,14 @@
+package com.pmo.swf1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Daftar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar);
+    }
+}
